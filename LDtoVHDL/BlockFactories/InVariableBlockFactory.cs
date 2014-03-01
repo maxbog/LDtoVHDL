ce5fa13ec8c8a54e35815ab75a22bed2b73ef21a
Maksymilian Boguń <mbogun@gmail.com>
2014-03-01 19:57:43 +0100

    InVariableBlockFactory and OutVariableBlockFactory

---
+++ LDtoVHDL/BlockFactories/InVariableBlockFactory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using LDtoVHDL.Blocks;
+
+namespace LDtoVHDL.BlockFactories
+{
+	class InVariableBlockFactory : BaseBlockFactory
+	{
+		public override IEnumerable<string> BlockTypes { get { yield return InVariableBlock.TYPE; } }
+		public override BaseBlock CreateBlock(XElement xBlock, Environment env)
+		{
+			var varName = GetVariableName(xBlock);
+			return new InVariableBlock(GetBlockLocalId(xBlock), varName, env.Variables[varName].SignalType);
+		}
+
+		private string GetVariableName(XElement xBlock)
+		{
+			return (string)xBlock.Descendants("expression".XName()).Single();
+		}
+	}
+}
---
+++ LDtoVHDL/BlockFactories/OutVariableBlockFactory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using LDtoVHDL.Blocks;
+
+namespace LDtoVHDL.BlockFactories
+{
+	class OutVariableBlockFactory : BaseBlockFactory
+	{
+		public override IEnumerable<string> BlockTypes { get { yield return OutVariableBlock.TYPE; } }
+		public override BaseBlock CreateBlock(XElement xBlock, Environment env)
+		{
+			var varName = GetVariableName(xBlock);
+			return new OutVariableBlock(GetBlockLocalId(xBlock), varName, env.Variables[varName].SignalType);
+		}
+
+		private string GetVariableName(XElement xBlock)
+		{
+			return (string)xBlock.Descendants("expression".XName()).Single();
+		}
+	}
+}
