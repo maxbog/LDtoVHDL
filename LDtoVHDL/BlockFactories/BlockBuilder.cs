709332fe8df6cd1662a9a04ad50e9069fddec237
Maksymilian Boguń <mbogun@gmail.com>
2014-03-09 22:44:06 +0100

    usunięte code issues

--- LDtoVHDL/BlockFactories/BlockBuilder.cs
+++ LDtoVHDL/BlockFactories/BlockBuilder.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Xml.Linq;
+﻿using System.Xml.Linq;
 using LDtoVHDL.Blocks;
 
 namespace LDtoVHDL.BlockFactories
--- LDtoVHDL/EnumerableEx.cs
+++ LDtoVHDL/EnumerableEx.cs
@@ -8,7 +8,7 @@ namespace LDtoVHDL
 	{
 		public static IEnumerable<Tuple<Type, IEnumerable<T>>> WithAttributes<T>(this IEnumerable<Type> @this)
 		{
-			return @this.Select(type => Tuple.Create<Type, IEnumerable<T>>(type, GetAttributesOfType<T>(type))).Where(tuple => tuple.Item2.Any());
+			return @this.Select(type => Tuple.Create(type, GetAttributesOfType<T>(type))).Where(tuple => tuple.Item2.Any());
 		}
 
 		public static IEnumerable<T> GetAttributesOfType<T>(this Type type)
