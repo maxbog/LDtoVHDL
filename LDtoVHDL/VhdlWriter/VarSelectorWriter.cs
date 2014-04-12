c61532c8894e78f3e578d432b33d1c4460580897
Maksymilian Boguń <mbogun@gmail.com>
2014-04-12 20:09:04 +0200

    lepszy VAR_SELECTOR

--- LDtoVHDL/VhdlWriter/VarSelectorWriter.cs
+++ LDtoVHDL/VhdlWriter/VarSelectorWriter.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Text;
 using LDtoVHDL.Model;
 using LDtoVHDL.Model.Blocks;
 
@@ -16,28 +17,63 @@ namespace LDtoVHDL.VhdlWriter
 
 		public override string GetVhdlType(BaseBlock block)
 		{
-			var addBlock = (VarSelector)block;
-			return MakeTypedName("BLK_VAR_SELECTOR", addBlock.Output.SignalType);
+			var varSelector = (VarSelector)block;
+			return MakeTypedName(string.Format("BLK_VAR_SELECTOR{0}", GetSignalCount(varSelector)), varSelector.Output.SignalType);
 		}
 
-		protected override IEnumerable<Tuple<string, string>> GetGenericMapping(BaseBlock block)
+		private static int GetSignalCount(VarSelector varSelector)
 		{
-			var varSelector = (VarSelector) block;
-			var inputsBus = ((BusType)varSelector.Inputs.SignalType);
-			yield return Tuple.Create("signal_count", inputsBus.SignalCount.ToString(CultureInfo.InvariantCulture));
+			var inputsBus = ((BusType) varSelector.Inputs.SignalType);
+			var signalCount = inputsBus.SignalCount;
+			return signalCount;
 		}
 
 		public override string GetComponentReference(BaseBlock block)
 		{
 			var varSelector = (VarSelector)block;
-			return PrepareTemplateForOutput(TemplateResolver.GetWithReplacements("BlockReference/BLK_VAR_SELECTOR.ref", new Dictionary<string, string> { { "type", SignalTypeWriter.GetName(varSelector.Output.SignalType) } }));
+			var signalCount = GetSignalCount(varSelector);
+			return PrepareTemplateForOutput(TemplateResolver.GetWithReplacements("BlockReference/BLK_VAR_SELECTOR.ref", 
+				new Dictionary<string, string>
+				{
+					{ "type", SignalTypeWriter.GetName(varSelector.Output.SignalType) },
+					{"signal_count", signalCount.ToString(CultureInfo.InvariantCulture)},
+					{"max_vector_idx", (signalCount-1).ToString(CultureInfo.InvariantCulture)}
+				}));
 		}
 
 		public override string GetDefinition(BaseBlock block)
 		{
 			var varSelector = (VarSelector)block;
+			var signalCount = GetSignalCount(varSelector);
 			return TemplateResolver.GetWithReplacements("BlockDefinition/BLK_VAR_SELECTOR.vhd",
-					new Dictionary<string, string> {{"type", SignalTypeWriter.GetName(varSelector.Output.SignalType)}});
+				new Dictionary<string, string>
+				{
+					{"type", SignalTypeWriter.GetName(varSelector.Output.SignalType)},
+					{"signal_count", signalCount.ToString(CultureInfo.InvariantCulture)},
+					{"max_vector_idx", (signalCount-1).ToString(CultureInfo.InvariantCulture)},
+					{"input_chooser", MakeInputChooser(varSelector)}
+				});
+		}
+
+		private string MakeInputChooser(VarSelector varSelector)
+		{
+			var signalCount = GetSignalCount(varSelector);
+			var result = new StringBuilder();
+			for (int i = 0; i < signalCount; ++i)
+			{
+				var inChooser = "";
+				for (int j = 0; j < i; ++j)
+				{
+					inChooser += '0';
+				}
+				inChooser += '1';
+				for (int j = i+1; j < signalCount; ++j)
+				{
+					inChooser += '-';
+				}
+				result.AppendFormat("INS({0}) when std_match(CONTROL, \"{1}\") else\n         ", signalCount - i - 1, inChooser);
+			}
+			return result.ToString();
 		}
 	}
 }
