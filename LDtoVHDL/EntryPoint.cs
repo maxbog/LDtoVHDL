1f4ce9c3dd78162fbc491c1ee97f228f388cc239
Maksymilian Boguń <mbogun@gmail.com>
2014-05-05 18:09:07 +0200

    plik wejściowy i katalog wyjściowy pobrany z argumentów

--- LDtoVHDL/EntryPoint.cs
+++ LDtoVHDL/EntryPoint.cs
@@ -1,8 +1,10 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Xml.Linq;
 using LDtoVHDL.Parsing;
 using LDtoVHDL.Translation;
 using LDtoVHDL.VhdlWriter;
+using NDesk.Options;
 
 namespace LDtoVHDL
 {
@@ -10,9 +12,34 @@ namespace LDtoVHDL
 	{
 		public static void Main(string[] args)
 		{
-			const string outputBaseDir = @"d:\dokumenty\copy\praca magisterska\silnik_vhdl\vhd\";
 
-			var parser = new PlcOpenParser(XDocument.Load(File.OpenRead(@"d:\dokumenty\copy\praca magisterska\silnik\plc.xml")));
+			string inputFile = null;
+			string outputBaseDir = null;
+			var optionsParser = new OptionSet
+			{
+				{"i|input=", "Path to plc.xml file", v => inputFile = v},
+				{"o|output=", "Base output directory, where vhd files will be stored", v => outputBaseDir = v}
+			};
+
+			try
+			{
+				optionsParser.Parse(args);
+			}
+			catch (OptionException e)
+			{
+				Console.Write("LdToVHDL: ");
+				Console.WriteLine(e.Message);
+				Console.WriteLine("Try `LdToVHDL --help' for more information.");
+				return;
+			}
+
+			if (inputFile == null || outputBaseDir == null)
+			{
+				Console.WriteLine("ERROR: Both input file and base output dir options are required");
+				return;
+			}
+
+			var parser = new PlcOpenParser(XDocument.Load(File.OpenRead(inputFile)));
 
 			var program = parser.Parse();
 			var translator = new Translator();
